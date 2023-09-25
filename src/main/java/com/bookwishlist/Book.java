39d8a532ceff6b997df1c0c3d4e322c82d8c5f92
Brooke Hartley <82181143+brooke-hartley@users.noreply.github.com>
2023-09-25 17:05:57 -0400

    Created a class object for Book

---
+++ src/main/java/com/bookwishlist/Book.java
@@ -0,0 +1,11 @@
+package com.bookwishlist;
+
+public class Book {
+    String addTitle;
+    String addAuthor;
+
+    public void newBook(String title, String author){
+        this.addTitle = title;
+        this.addAuthor = author;
+    }
+}
